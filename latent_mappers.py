19799953b3e55406d0652301a883bc385f2f4b73
george <georgiy.grigorev@reface.ai>
2022-05-09 17:53:58 +0300

    fix

--- latent_mappers.py
+++ latent_mappers.py
@@ -19,20 +19,20 @@ class ModulationModule(Module):
         self.layernum = layernum
         self.fc = Linear(512, 512)
         self.norm = LayerNorm([self.layernum, 512], elementwise_affine=False)
-        self.gamma_function = Sequential(Linear(512, 512), LayerNorm([512]), LeakyReLU(), Linear(512, 512))
-        self.beta_function = Sequential(Linear(512, 512), LayerNorm([512]), LeakyReLU(), Linear(512, 512))
+        # self.gamma_function = Sequential(Linear(512, 512), LayerNorm([512]), LeakyReLU(), Linear(512, 512))
+        # self.beta_function = Sequential(Linear(512, 512), LayerNorm([512]), LeakyReLU(), Linear(512, 512))
         self.leakyrelu = LeakyReLU()
 
-    def forward(self, x, embedding):
+    def forward(self, x): #, embedding):
         x = self.fc(x)
         x = self.norm(x)
-        if embedding is not None:
-            gamma = self.gamma_function(embedding.float())
-            beta = self.beta_function(embedding.float())
-            out = x * (1 + gamma) + beta
-        else:
-            out = x
-        out = self.leakyrelu(out)
+        # if embedding is not None:
+        #     gamma = self.gamma_function(embedding.float())
+        #     beta = self.beta_function(embedding.float())
+        #     out = x * (1 + gamma) + beta
+        # else:
+        #     out = x
+        out = self.leakyrelu(x)
         return out
 
 
@@ -43,10 +43,10 @@ class SubMapper(Module):
         self.pixelnorm = PixelNorm()
         self.modulation_module_list = nn.ModuleList([ModulationModule(self.layernum) for i in range(5)])
 
-    def forward(self, x, embedding=None):
+    def forward(self, x):
         x = self.pixelnorm(x)
         for modulation_module in self.modulation_module_list:
-            x = modulation_module(x, embedding)
+            x = modulation_module(x)
         return x
 
 
@@ -56,7 +56,7 @@ class Mapper(Module):
         self.course_mapping = SubMapper(4)
         self.medium_mapping = SubMapper(4)
 
-    def forward(self, x, clip_embedding=None):
+    def forward(self, x):
         """
         :param x: (batch_size, 8, 512) – style space embedding
         :param clip_embedding:
